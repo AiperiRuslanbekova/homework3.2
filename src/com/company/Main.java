6ace62839599c6216db2505bf12c64a08b9301c6
Aiperi <aruslanbekova1@gmail.com>
2021-01-02 23:23:50 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] array = new int[] {2,4,6,1,3,5,7};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
